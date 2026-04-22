986141e2a1de1f1d9a25431d965bafbf3458a2a6
Megha Jayalath <113291487+MeghaJayalath@users.noreply.github.com>
2026-04-23 01:08:07 +0530

    Revert

--- scripts/wp-portfolio-cpt.php
+++ scripts/wp-portfolio-cpt.php
@@ -187,13 +187,6 @@ function zot_portfolio_styles() {
             font-size: 1rem;
             font-weight: 300;
             line-height: 1.6;
-            /* Truncate after 3 lines - Forceful */
-            display: -webkit-box !important;
-            -webkit-line-clamp: 3 !important;
-            -webkit-box-orient: vertical !important;
-            overflow: hidden !important;
-            text-overflow: ellipsis;
-            max-height: 4.8em; /* 3 lines * 1.6 line-height */
         }
 
         .zot-card-line {
@@ -271,8 +264,6 @@ function zot_portfolio_grid_shortcode() {
         $query->the_post();
         $thumb = get_the_post_thumbnail_url(get_the_ID(), 'large') ?: 'https://ui.shadcn.com/placeholder.svg';
         $stack = get_post_meta(get_the_ID(), 'stack', true);
-        $excerpt = wp_strip_all_tags(get_the_excerpt());
-        $excerpt = wp_trim_words($excerpt, 25, '...');
         
         $output .= sprintf(
             '<a href="%s" class="zot-card">
@@ -287,7 +278,7 @@ function zot_portfolio_grid_shortcode() {
             get_the_title(),
             $stack ? explode(',', $stack)[0] : 'Project',
             get_the_title(),
-            $excerpt
+            get_the_excerpt()
         );
     }
     
